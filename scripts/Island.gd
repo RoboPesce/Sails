8ce7b9cd7ffde696068500c1e67e39611cf94136
Stefano Corno <stlecorno@gmail.com>
2023-12-18 00:29:42 -0800

    cloud mesh

--- scripts/Island.gd
+++ scripts/Island.gd
@@ -3,10 +3,11 @@ extends StaticBody
 var global = null
 
 # Cached particles
-onready var smokeShader : Particles = $Smoke.process_material
+onready var smokeShader : ShaderMaterial = $Smoke.process_material
 
 func _ready():
 	if(!global): global = get_parent()
 
 func _physics_process(_delta):
 	smokeShader.set_shader_param("wind_dir", global.wind)
+	smokeShader.set_shader_param("wind_strength", global.wind_strength)
