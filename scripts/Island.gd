b549d0a8b788517e505c1cca870fcce817f9a1e8
Stefano Corno <stlecorno@gmail.com>
2023-12-17 20:23:35 -0800

    writing smoke shader code from scratch

--- scripts/Island.gd
+++ scripts/Island.gd
@@ -3,12 +3,10 @@ extends StaticBody
 var global = null
 
 # Cached particles
-onready var smoke : Particles = $Smoke
+onready var smokeShader : Particles = $Smoke.process_material
 
 func _ready():
 	if(!global): global = get_parent()
 
 func _physics_process(_delta):
-	smoke.process_material.direction = global.wind
-	smoke.process_material.linear_accel = global.wind_strength * 100
-	pass
+	smokeShader.set_shader_param("wind_dir", global.wind)
