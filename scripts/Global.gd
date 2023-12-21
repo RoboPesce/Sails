085fcf85ae2ef75e31ab2fe1759da12bef063c2b
Stefano Corno <stlecorno@gmail.com>
2023-12-20 20:16:55 -0800

    Tweaks

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -9,13 +9,13 @@ onready var mousecast = $MouseCast
 var mouse_pos : Vector3
 
 # wind control
-var wind : Vector3 = Vector3(1, 0, 0)
+var wind : Vector3 = Vector3(0, 0, -1)
 var wind_strength : float
 # wind params
 export var wind_rotational_velocity : float = .6
 export var wind_strength_velocity : float = .2
 export var wind_max : float = 1
-export var wind_min : float = 0.1
+export var wind_min : float = 0.2
 
 # boat control
 var is_compass_cardinal : bool
